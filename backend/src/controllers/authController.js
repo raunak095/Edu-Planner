340f72c95857b153b29b94530721398adc7bb6f3
raunak095 <draunak979@gmail.com>
2026-05-10 13:07:45 -0700

    Gmail SMTP config

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -11,20 +11,19 @@ const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
   port: 465,
   secure: true,
-  family: 4,
 
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
-  tls: {
+  /*tls: {
     rejectUnauthorized: false,
-  },
+  },*/
 
   // ✅ FIXED TIMEOUT ISSUES
-  connectionTimeout: 10000,
+  /*connectionTimeout: 10000,
   greetingTimeout: 10000,
-  socketTimeout: 10000,
+  socketTimeout: 10000,*/
 });
 
 transporter.verify((error, success) => {
