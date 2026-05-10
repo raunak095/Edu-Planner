2d5c6fcddb282ff458f26bae0794be2d06bb3435
raunak095 <draunak979@gmail.com>
2026-05-10 12:53:05 -0700

    Fixed Gmail IPv6 issue

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -8,11 +8,17 @@ dotenv.config();
 
 // ================= EMAIL TRANSPORT =================
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
+  family: 4,
 
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
+  },
+  tls: {
+    rejectUnauthorized: false,
   },
 
   // ✅ FIXED TIMEOUT ISSUES
